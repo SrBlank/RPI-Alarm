ca9a8e31dcebc425371db6e6fe17bb70fdfd9584
Saad Rafiq <sar287@txstate.edu>
2022-11-25 21:34:12 -0600

    user able to add time and configured checkboxes

--- src/web_server.py
+++ src/web_server.py
@@ -1,28 +1,69 @@
-import flask
+from flask import Flask, render_template, redirect, url_for, request, flash
+from dotenv import load_dotenv, find_dotenv
 import os
+load_dotenv(find_dotenv())
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+app.secret_key = os.getenv('app_secret')
+
+list_of_alarms = ['18:45', '01:00', '11:11', '01:20']
+alarms_selected = ['18:45', '01:00']
+alarms_selected_old = ['18:45', '01:00']
 
 @app.route("/")
 def hello_world():
-    return flask.render_template("index.html")
+    return render_template(
+        "index.html",
+        alarms_list = list_of_alarms,
+        alarms_selcted = alarms_selected
+        )
 
-@app.route('/data/', methods = ['POST','GET'])
-def data():
-    if flask.request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if flask.request.method == 'POST':
-        form_data = flask.request.form
-        print(form_data)
-        return flask.render_template('index.html',form_data = form_data)
+@app.route("/update_alarms", methods=['POST'])
+def update_alarms():
+    form_data = request.form
+    form_checked = request.form.getlist("checkbox")
+    form_checkedN = request.form.getlist("checkboxN")
+    alarms_selected.clear()
+
+    for j in form_checkedN:
+        alarms_selected.append(j)
+    for k in form_checked:
+        alarms_selected.append(k)
+    
+    return redirect(url_for("hello_world"))
+
+
+@app.route("/process_time", methods = ['POST'])
+def process_time():
+    form_data = request.form
+
+    for i in list_of_alarms:
+        if i == form_data['time_box']:
+            flash('duplicate')
+            return redirect(url_for("hello_world"))
+
+    list_of_alarms.append(form_data['time_box'])
+    sort_list()
+    
+    return redirect(url_for("hello_world"))
+
+def sort_list():
+    for i in range(1,len(list_of_alarms)):
+        for j in range(0, len(list_of_alarms)-1):
+            hours_current = list_of_alarms[j][0] + list_of_alarms[j][1]
+            minutes_current = list_of_alarms[j][3] + list_of_alarms[j][4]
+            hours_next = list_of_alarms[j+1][0] + list_of_alarms[j+1][1]
+            minutes_next = list_of_alarms[j][3] + list_of_alarms[j][4]
+
+            if int(hours_current) >= int(hours_next) :
+                if int(minutes_current) <= int(minutes_next):
+                    temp = hours_current + ':' + minutes_current
+                    list_of_alarms[j] = list_of_alarms[j+1]
+                    list_of_alarms[j+1] = temp
+    
+    print(list_of_alarms)
 
 if __name__=="__main__":
     app.run()
 
-
-"""
-    <form action="/data" method = "POST">
-      <h2>New Alarm</h2>
-      <p>Enter Time <input type = "text" name = "Hours" /></p>
-      <p><input type = "submit" value = "Submit" /></p>
-"""
+ 
