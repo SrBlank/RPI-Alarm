4cca4ea9cdff42b654098fb60261d811195aa885
Saad Rafiq <sar287@txstate.edu>
2023-01-21 17:36:16 -0600

    updated alarm_class.py

--- src/alarm_class.py
+++ src/alarm_class.py
@@ -1,4 +1,8 @@
 
 class Alarm():
     def __init__():
-        pass
+        self.time = "--:--"
+        self.playtime = 3
+        self.button = True
+        self.alarm_sound = "./alarm_sounds/generic_alarm.mp3" 
+        
