8a2e11f0b35ce734cf5af64c45dbfe13c7a53668
Saad Rafiq <sar287@txstate.edu>
2023-01-25 14:38:26 -0600

    mp3 is now allowed to play fully, added more alarms

--- src/alarm_class.py
+++ src/alarm_class.py
@@ -1,10 +1,15 @@
+import ffmpeg
 
 class Alarm(object):
-    def __init__(self, time, playtime=3, alarm_sound="generic_alarm.mp3", enable_button=True):
+    def __init__(self, time, playtime=0, alarm_sound="generic_alarm.mp3", enable_button=True):
         self.time = time
         self.time_diff = None
-        self.playtime = playtime
         self.alarm_sound = "./alarm_sounds/" + alarm_sound
+
+        if playtime == "":
+            playtime = int(float(ffmpeg.probe(self.alarm_sound)['format']['duration']))
+        self.playtime = playtime
+        
         self.button = enable_button
         
     def __repr__(self):
