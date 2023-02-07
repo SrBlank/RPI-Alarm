fc1d6f66a4c590f3b5a6985c55eef728e2284055
= <rafiqsaad500@gmail.com>
2023-02-07 15:58:15 -0600

    alarm.py now uses pygame to play audio as to suport different platforms, keyboard intterupts are now accepted, button input intialization will handle button input for systems with and without gpio, improved logic for playing alarm

--- src/alarm.py
+++ src/alarm.py
@@ -3,6 +3,8 @@ import time
 import pickle
 import logging
 import signal
+import pygame
+import keyboard
 
 from datetime import datetime
 from subprocess import (
@@ -30,6 +32,8 @@ logger = logging.getLogger()
 logger.info("Starting Log File")
 
 """ INITILIZATION """
+pygame.init() # move this at a later time
+
 diff_array = []
 diff_arr_time_curr = 0
 diff_arr_time_prev = 0
@@ -39,43 +43,50 @@ loop_counter = 0
 MINUTES_IN_DAY = 1440
 ALARM_PLAYTIME = 5
 ALARM_TO_PLAY = "./alarm_sounds/generic_alarm.mp3"
-if os.name == 'posix':
-    ENABLE_BUTTON = True
-else:
-    ENABLE_BUTTON = False
 GPIO_INPUT_PIN = 10
 
-global STAY_IN_LOOP
-STAY_IN_LOOP = True
 
-# GPIO Initialization
-def alarm_stop_callback(channel):
-    logger.info("Button Pressed")
+"""
+Alarm Control Functions
+"""
+#
+# Function will handle intterupt from GPIO or keyboard
+#
+def interrupt_handler(channel=None):
+    logger.critical("Interrupt Detected")
+    pygame.mixer.music.stop()
 
-    global STAY_IN_LOOP
-    STAY_IN_LOOP = False
+#
+# Function will play alarm for x time or loop until intterupt
+#
+def play_alarm_timer(alarm_instance):
+    pygame.mixer.music.load(alarm_instance.alarm_sound)
+    pygame.mixer.music.play(loops=-1)
+    if(alarm_instance.input == "Timer"):
+        time.sleep(int(alarm_instance.playtime))
+        pygame.mixer.music.stop()
 
-    try:
-        process_id = int(check_output(["pidof", "-s", "mpg123"]))
-        os.system("kill -9 " + str(process_id))
-        logger.info("Process SKilled Sucessfully")
-    except CalledProcessError:
-        print("Process Does Not Exist")
-
+"""
+Button input intialization
+"""
 try:
+    logger.info("Attempting to intialize GPIO")
     import RPi.GPIO as GPIO
-
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(GPIO_INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(GPIO_INPUT_PIN, GPIO.RISING, callback=alarm_stop_callback)
+    GPIO.add_event_detect(GPIO_INPUT_PIN, GPIO.RISING, callback=interrupt_handler)
     logger.info("Intiliazed Variables and GPIO")
 except (RuntimeError, ModuleNotFoundError):
-    logger.info("Intilizaed Variables, button is disabled")
+    logger.info("Failed to intialize GPIO, keyboard input will be used")
+    keyboard.add_hotkey("esc", interrupt_handler)
+    logger.info("Intilizaed Variables and Keyboard")
 
+
+""" 
+LISTFILE.DATA INTILIZATION
+"""
 logger.info("Waiting for listfile.data to be ready")
-
-""" LISTFILE.DATA INTILIZATION"""
 while not os.path.exists("listfile.data"):
     time.sleep(1)
 if os.path.isfile("listfile.data"):
@@ -83,20 +94,11 @@ if os.path.isfile("listfile.data"):
 else:
     raise ValueError("listfile.data isn't a file!")
 
+
+""" 
+MAIN LOOP 
+"""
 logger.info("listfile.data is ready, starting loop")
-
-
-def playAlarm(alarm_instance):
-    proc = Popen(["mpg123", alarm_instance.alarm_sound])
-    try:
-        outs, errs = proc.communicate(timeout=int(alarm_instance.playtime))
-    except TimeoutExpired:
-        proc.kill()
-        outs, errs = proc.communicate()
-    logger.critical("** ALARM **")
-
-
-""" MAIN LOOP """
 while True:
     loop_counter = loop_counter + 1
 
@@ -130,7 +132,6 @@ while True:
 
         # iterate by counter and check if the time matches then play sound if so for ALARM_PLAYTIME seconds
         if counter < len(diff_array):
-            STAY_IN_LOOP = True  # Reset loop variable for next alarm
             alarm_instance = diff_array[counter]
             alarm_time = diff_array[counter].time  # [1]
             d = datetime.now()
@@ -141,13 +142,12 @@ while True:
                 counter = counter + 1
                 diff_array = sort_arr_time(diff_array)
 
-                if (alarm_instance.input == "Button") and ENABLE_BUTTON:
-                    logger.critical("** ALARM DONE WITH BUTTON **")
-                    while STAY_IN_LOOP:  # Repeat audio until button has been pressed
-                        playAlarm(alarm_instance)
-                else:
-                    logger.critical("** ALARM DONE NO BUTTON **")
-                    playAlarm(alarm_instance)
+                logger.critical("** ALARM DONE -- INPUT TYPE -- " + (alarm_instance.input).upper() + " **")
+                if(alarm_instance.input == "Sensor"):
+                    # TODO
+                    pass
+                elif alarm_instance.input == "Button" or alarm_instance.input == "Timer":
+                    play_alarm_timer(alarm_instance)          
 
             else:
                 pass
